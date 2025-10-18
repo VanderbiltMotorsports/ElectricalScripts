b87f4aa3851251cac87f1a184e996636cbb2a2c4
M-ThomasThaickal <thomasmanu.7487@gmail.com>
2025-10-18 22:57:13 +0100

    preliminary pi wheel speed implementation - untested

--- PiDAQ/wheel_sensor.py
+++ PiDAQ/wheel_sensor.py
@@ -1,10 +1,59 @@
+
 import socket
 import RPi.GPIO as GPIO
 import time
 
-BUTTON_PIN = 
+HALL_PIN = 3
+DEBOUNCE_DELAY = 0.027
+
+pulse_interval = 0
+rising_time = 0
+falling_time = 0
+
 UDP_IP = "10.74.1.33"
 UDP_PORT = 5005
 
 # make wheel speed sensor code, check button code to see how to send 
 # data over UDP
+
+
+def hall_rising(channel):
+	global pulse_interval, rising_time, falling_time
+
+	now = time.time()
+
+	if (now - falling_time) > DEBOUNCE_DELAY:
+		pulse_interval = now - rising_time
+		rising_time = now
+		falling_time = now
+
+def setup():
+	GPIO.setmode(GPIO.BOARD)
+	GPIO.setup(HALL_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+	GPIO.add_event_detect(HALL_PIN, GPIO.RISING, callback=hall_rising)
+
+	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	print(f"Sending to {UDP_IP}:{UDP_PORT}")
+
+
+def loop():
+	global pulse_interval
+
+	while True:
+		if pulse_interval > 0:
+			rpm = 60.0 / pulse_intereval
+			message = f"RPM: {rpm:2f}"
+			sock.sendto(message.encode(), (UDP_IP, UDP_PORT))
+			print(f"Sent: {message}")
+		time.sleep(0.1)
+
+if __name__ == "__main__":
+	try:
+		setup()
+		loop()
+	except KeyobardInterrupt:
+		print("\nExiting...")
+	finally:
+		GPIO.cleanup()
+		sock.close()
